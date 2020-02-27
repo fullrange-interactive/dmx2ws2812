23f1762e0c81a16fa7494a34574c089cc5ba9e05
floriansegginger <florian@fullrange.ch>
2020-02-27 10:15:57 +0100

    Basic functionality

--- embedded/embedded.ino
+++ embedded/embedded.ino
@@ -4,106 +4,254 @@
 #include "FRILCD.h"
 #include "Conceptinetics.h"
 
-#define SCREEN_TX		3
-#define SCREEN_RX		4
+#define SCREEN_TX       7
+#define SCREEN_RX       6
 
 #include <FastLED.h>
+#include <Rotary.h>
+
 // RGB leds specs
-#define LED_PIN       5
-#define RING          0
-#define STRIP         150
-#define NUM_LEDS      RING + STRIP
+#define LED_PIN         5
+#define NUM_LEDS        20
+#define LED_CHANNELS    3
+
+// Rotary encoder specs
+#define ENC_A           13
+#define ENC_B           11
+#define ENC_PB          12
 
 #define LED_TYPE      WS2812
 #define COLOR_ORDER   GRB
 
+// Timing constants
+#define WAIT_DMX        30
+#define WAIT_LEDS       30
+#define WAIT_LCD        500
+#define WAIT_ENCODER    0
+
+#define DEBOUNCE_ENC_PB 50
+
+#define LCD_WAKE_DURATION 5000
+
 CRGB leds[NUM_LEDS];
 
 FRILCD lcd(SCREEN_RX, SCREEN_TX);
-DMX_Slave dmxIn ( NUM_LEDS * 3 );
+
+DMX_Slave dmxIn ( NUM_LEDS * LED_CHANNELS );
+
+Rotary rotary = Rotary(ENC_A, ENC_B);
+
+unsigned char DMXValues[NUM_LEDS * LED_CHANNELS];
+
+bool lcdNeedsUpdate = false;
+int testGlobal = 0;
+
+struct EncoderAction {
+    char direction;
+    bool pressed;
+};
+
+struct Config {
+    int DMXAddress;
+};
+
+Config currentConfig;
 
 void setup() {
 
-	pinMode(13, OUTPUT);
+    currentConfig.DMXAddress = 1;
 
-	pinMode(LED_PIN, OUTPUT);
+    // Inputs
+    pinMode(ENC_PB, INPUT_PULLUP);
+    pinMode(ENC_A, INPUT_PULLUP);
+    pinMode(ENC_B, INPUT_PULLUP);
 
-	Serial.begin(9600);
+    Serial.begin(9600);
 
-	lcd.start();
+    // Outputs
+    pinMode(LED_PIN, OUTPUT);
 
-	  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS)
-	  .setCorrection(TypicalLEDStrip);
+    // LCD
+    lcd.start();
+    lcd.clear();
 
-	  FastLED.setBrightness(255);
-	  FastLED.show();
+    // LED Strip
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS)
+    .setCorrection(TypicalLEDStrip);
+    FastLED.setBrightness(255);
 
-	// lcd.setBacklight(true);
+    // DMX
+    dmxIn.enable();
+    dmxIn.setStartAddress(currentConfig.DMXAddress);
 
-	// lcd.setSplashScreen("FRI DMX-WS2812", "v1.0");
+    delay(1000);
 
-	lcd.clear();
-	lcd.writeLine1("DMX Value:");
-	lcd.writeLine2("0");
-
-	dmxIn.enable();
-	dmxIn.setStartAddress(1);
-
-	delay(1000);
+    lcd.setBacklight(false);
 }
 
-void updateStrip(DMX_Slave& slave) {
-  for (uint16_t i = RING; i < NUM_LEDS; i++) {
+void updateDMX(unsigned long dt) {
 
-	int rIn = dmxIn.getChannelValue(i*3 + 1);
-	int gIn = dmxIn.getChannelValue(i*3 + 2);
-	int bIn = dmxIn.getChannelValue(i*3 + 3);
-    leds[i] = CRGB(rIn,
-                   gIn,
-                   bIn);
+    static unsigned long totalDT = 0;
+    static unsigned char lastDMXAddress = currentConfig.DMXAddress;
 
-  }
+    totalDT += dt;
 
-  FastLED.setBrightness(255);
-  FastLED.show();
+    if (lastDMXAddress != currentConfig.DMXAddress) {
+        dmxIn.setStartAddress(currentConfig.DMXAddress);
+        lastDMXAddress = currentConfig.DMXAddress;
+    }
+
+    if (totalDT > WAIT_DMX) {
+        for (int led = 0; led < NUM_LEDS; led++) {
+            for (int channel = 0; channel < LED_CHANNELS; channel++ ) {
+                DMXValues[led * LED_CHANNELS + channel] = dmxIn.getChannelValue(led * LED_CHANNELS + channel + 1);
+            }
+        }
+
+        totalDT = 0;
+    }
 
 }
 
-int wait = 0;
+void updateLEDs(unsigned long dt) {
+
+    static unsigned long totalDT = 0;
+    totalDT += dt;
+
+    if (totalDT > WAIT_LEDS) {
+        for (int led = 0; led < NUM_LEDS; led++) {
+            unsigned char r = DMXValues[led * LED_CHANNELS];
+            unsigned char g = DMXValues[led * LED_CHANNELS + 1];
+            unsigned char b = DMXValues[led * LED_CHANNELS + 2];
+            leds[led] = CRGB(r, g, b);
+        }
+
+        FastLED.setBrightness(255);
+        FastLED.show();
+
+        totalDT = 0;
+    }
+
+}
+
+EncoderAction updateEncoder(unsigned long dt) {
+
+    static unsigned long totalDT = 0;
+    totalDT += dt;
+
+    static bool lastPressed = false;
+    static unsigned long lastPressedTime = 0;
+
+    EncoderAction result;
+
+    if (totalDT > WAIT_ENCODER) {
+        
+        result.direction = rotary.process();
+
+        bool pinActive = digitalRead(ENC_PB);
+        // if (pressed != lastPressed) {
+        //     lastPressedTime = millis();
+        // }
+        // if ((millis - lastPressedTime) > DEBOUNCE_ENC_PB) {
+        //     result.pressed = true;
+        // }
+        // lastPressed = pressed;
+
+        result.pressed = !pinActive;
+
+        totalDT = 0;
+    }
+
+    return result;
+
+}
+
+void updateLCD(unsigned long dt, EncoderAction action) {
+
+    static unsigned long screenWakeDt = 0;
+    static bool isWoke = false;
+    screenWakeDt += dt;
+
+
+    // Keep scren woke
+    if (isWoke && (action.direction != DIR_NONE || action.pressed)) {
+        screenWakeDt = 0;
+    }
+
+    if (isWoke && action.direction != DIR_NONE) {
+
+        if (action.direction == DIR_CW) {
+            currentConfig.DMXAddress++;
+            if (action.pressed) {
+                currentConfig.DMXAddress += 9;
+            }
+        } else {
+            currentConfig.DMXAddress--;
+            if (action.pressed) {
+                currentConfig.DMXAddress -= 9;
+            }
+        }
+        if (currentConfig.DMXAddress > 512) {
+            currentConfig.DMXAddress = 1;
+        }
+        if (currentConfig.DMXAddress < 1) {
+            currentConfig.DMXAddress = 512;
+        } 
+
+        lcd.writeLine1("DMX Address:");
+        char buff[16];
+        sprintf(buff, "%d", currentConfig.DMXAddress);
+        lcd.writeLine2(buff);
+
+    }
+
+    if ((action.pressed || action.direction != DIR_NONE) && !isWoke) {
+
+        screenWakeDt = 0;
+        lcd.setBacklight(true);
+        isWoke = true;
+
+        delay(50);
+
+        lcd.writeLine1("DMX Address:");
+        char buff[16];
+        sprintf(buff, "%d", currentConfig.DMXAddress);
+        lcd.writeLine2(buff);
+
+        // lcd.writeLine1("DMX Address:");
+
+        // char buff[16];
+        // sprintf(buff, "%d", currentConfig.DMXAddress);
+        // lcd.writeLine2(buff);
+
+    }
+
+    if (isWoke && screenWakeDt > LCD_WAKE_DURATION) {
+        isWoke = false;
+        lcd.setBacklight(false);
+    }
+    //     lc
+    //     char buff[16];
+    //     itoa(testGlobal, buff, 10);
+    //     lcd.writeLine2(buff);
+
+    //     totalDT = 0;
+    //     lcdNeedsUpdate = false;
+    // }
+
+}
+
+unsigned long lastT = 0;
 
 void loop() {
 
-	// return;
+    unsigned long now = millis();
+    unsigned long dt = now - lastT;
+    lastT = now;
 
-	delay(50);
-	int dimIn = 255; //dmxIn.getChannelValue(1);
-	// int rIn = dmxIn.getChannelValue(1);
-	// int gIn = dmxIn.getChannelValue(2);
-	// int bIn = dmxIn.getChannelValue(3);
-	// int shutterIn = dmxIn.getChannelValue(5);
-
-	int val = dmxIn.getChannelValue(1);
-	char buffer[16];
-	itoa(val, buffer, 10);	// if (wait == 0) {
-
-	// 	lcd.writeLine1("DMX Value:");
-	// 	lcd.writeLine2(buffer);
-	// }
-	// wait++;
-	// if (wait == 20) {
-	// 	wait = 0;
-	// }
-
-
-
-	// if (val > 128) {
-	// 	digitalWrite(13, HIGH);
-	// } else {
-	// 	digitalWrite(13, LOW);
-	// }
-
-	updateStrip(dmxIn);
-
-	// delay(500);
-	
+    updateDMX(dt);
+    updateLEDs(dt);
+    EncoderAction action = updateEncoder(dt);
+    updateLCD(dt, action);
+    
 }
